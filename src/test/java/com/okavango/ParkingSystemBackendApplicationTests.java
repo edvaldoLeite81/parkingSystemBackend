ae036b34eb5b38f5d790e57af3b12f9351a03667
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-11-03 19:03:08 -0300

    created

---
+++ src/test/java/com/okavango/ParkingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okavango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
